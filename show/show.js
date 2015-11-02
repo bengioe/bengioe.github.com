e4738bf413fedb5dd8dffb1adf9767e26471985f
bengioe <bengioe@gmail.com>
2015-11-02 07:35:18 -0500

    d3

--- show/show.js
+++ show/show.js
@@ -220,4 +220,55 @@ function DAG(nodes, links, invisible_links, centerpos,centeredNode){
     section.force = force;
 }
 
+
+function groupLinksGraph(graph){
+
+    var section = document.currentScript.parentElement;
+
+    var width = 400, height = 400;
+
+    var color = d3.scale.category20();
+
+    var force = d3.layout.force()
+	.charge(-120)
+	.linkDistance(30)
+	.size([width, height]);
+
+    var svg = d3.select(section).append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+    force
+	.nodes(graph.nodes)
+	.links(graph.links)
+	.start();
+
+    var link = svg.selectAll(".link")
+	.data(graph.links)
+	.enter().append("line")
+	.attr("class", "link")
+	.style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+    var node = svg.selectAll(".node")
+	.data(graph.nodes)
+	.enter().append("circle")
+	.attr("class", "node")
+	.attr("r", 5)
+	.style("fill", function(d) { return color(d.group); })
+	.call(force.drag);
+
+    //node.append("title")
+    //	.text(function(d) { return d.name; });
+
+    force.on("tick", function() {
+	link.attr("x1", function(d) { return d.source.x; })
+	    .attr("y1", function(d) { return d.source.y; })
+	    .attr("x2", function(d) { return d.target.x; })
+	    .attr("y2", function(d) { return d.target.y; });
+
+	node.attr("cx", function(d) { return d.x; })
+	    .attr("cy", function(d) { return d.y; });
+    });
+}
+
 //setInterval(function(){location.reload();}, 1000)
