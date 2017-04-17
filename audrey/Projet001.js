83b7426dca4ddadbe4f46e40cd629bee875bc55c
Emmanuel Bengio <bengioe@gmail.com>
2017-04-16 22:57:20 -0400

    utf-8

--- audrey/Projet001.js
+++ audrey/Projet001.js
@@ -1,4 +1,4 @@
-//Changement du texte (div) pour générer une insulte
+//Changement du texte (div) pour gÃ©nÃ©rer une insulte
 
 function newpage1(){
 	var img = document.getElementById("maxime")
@@ -9,13 +9,13 @@ function insultemaxime () {
 	var textDiv = document.getElementById("monInsulte")
 	var hiddenMaxime = document.getElementById ("frontpage")
 	hiddenMaxime.style.display = "none"
-	insulte1 = ["un fucking", "le plus useless", "un caca de", "un osti de", "un espèce de", "un maudit", "un petit", "un tas de", 
-	"un amateur de", "un bidule pour", "un verre à moitié plein de", "un satané"]
-	insulte3 = ["citrouille", "patate", "lama", "lépreux", "mouette", "chimpanzé", "moufette"]
+	insulte1 = ["un fucking", "le plus useless", "un caca de", "un osti de", "un espÃ¨ce de", "un maudit", "un petit", "un tas de", 
+	"un amateur de", "un bidule pour", "un verre Ã  moitiÃ© plein de", "un satanÃ©"]
+	insulte3 = ["citrouille", "patate", "lama", "lÃ©preux", "mouette", "chimpanzÃ©", "moufette"]
 	var insulteIntermediaire = " " + insulte3 [Math.floor(Math.random()*insulte3.length)]
-	insulte2 = ["joueur de poker", "nain", "troglodyte", "poney miniature", "weakling intolérant au lactose", "arriéré mental aux yeux bleus", "cunt", "reject", "yogourt moisi", "déchet", "marde", "patapouf", 
-	"vomi de" + insulteIntermediaire, "jus de" + insulteIntermediaire, "pet", "fish", "compteur de grain de maïs", "twat", "sphincter de" + insulteIntermediaire, 
-	"débile", "retard", "noob", "weakling", "rat d'égoût", "contenant à" + insulteIntermediaire, "tabarnak de" + insulteIntermediaire, "poil de" + insulteIntermediaire]	
+	insulte2 = ["joueur de poker", "nain", "troglodyte", "poney miniature", "weakling intolÃ©rant au lactose", "arriÃ©rÃ© mental aux yeux bleus", "cunt", "reject", "yogourt moisi", "dÃ©chet", "marde", "patapouf", 
+	"vomi de" + insulteIntermediaire, "jus de" + insulteIntermediaire, "pet", "fish", "compteur de grain de maÃ¯s", "twat", "sphincter de" + insulteIntermediaire, 
+	"dÃ©bile", "retard", "noob", "weakling", "rat d'Ã©goÃ»t", "contenant Ã " + insulteIntermediaire, "tabarnak de" + insulteIntermediaire ]	
 	textDiv.innerHTML = "Tu es" + "<br>" + insulte1 [Math.floor(Math.random()*insulte1.length)]+ " " + insulte2 [Math.floor(Math.random()*insulte2.length)] + "<br>" + "!!! " + "<br>" + "&nbsp;"
 }
 function newpage2(){
@@ -27,12 +27,12 @@ function insultepatrick(){
 	var textDiv = document.getElementById("monInsulte")
 	var hiddenPatrick = document.getElementById ("frontpage")
 	hiddenPatrick.style.display = "none"
-	insulte1 = ["le pire", "un fucking", "le plus useless", "un caca de", "un osti de", "un espèce de", "un maudit", "un petit", 
-	"un amateur de", "un verre à moitié plein de", "un satané", "le pire", "le pire", "le pire", "le pire", "le pire"]
-	insulte3 = ["citrouille", "patate", "lama", "lépreux", "mouette", "chimpanzé", "moufette"]
+	insulte1 = ["le pire", "un fucking", "le plus useless", "un caca de", "un osti de", "un espÃ¨ce de", "un maudit", "un petit", 
+	"un amateur de", "un verre Ã  moitiÃ© plein de", "un satanÃ©", "le pire", "le pire", "le pire", "le pire", "le pire"]
+	insulte3 = ["citrouille", "patate", "lama", "lÃ©preux", "mouette", "chimpanzÃ©", "moufette"]
 	var insulteIntermediaire = " " + insulte3 [Math.floor(Math.random()*insulte3.length)]
-	insulte2 = ["starfish","cycliste", "chou-fleur", "tueur de plantes", "coloc", "gros obèse morbide",  "criss d'asthmatique", "cunt", "reject", "yogourt moisi", "déchet", "tas de marde", "patapouf", 
-	"vomi de" + insulteIntermediaire, "jus de" + insulteIntermediaire, "pet", "fish", "compteur de grain de maïs", "twat", "sphincter de" + insulteIntermediaire, 
-	"débile", "retard", "noob", "weakling", "rat d'égoût", "contenant à" + insulteIntermediaire, "tabarnak de" + insulteIntermediaire, "poil de" + insulteIntermediaire]
+	insulte2 = ["starfish","cycliste", "chou-fleur", "tueur de plantes", "coloc", "gros obÃ¨se morbide",  "criss d'asthmatique", "cunt", "reject", "yogourt moisi", "dÃ©chet", "tas de marde", "patapouf", 
+	"vomi de" + insulteIntermediaire, "jus de" + insulteIntermediaire, "pet", "fish", "compteur de grain de maÃ¯s", "twat", "sphincter de" + insulteIntermediaire, 
+	"dÃ©bile", "retard", "noob", "weakling", "rat d'Ã©goÃ»t", "contenant Ã " + insulteIntermediaire, "tabarnak de" + insulteIntermediaire, "poil de" + insulteIntermediaire]
 	textDiv.innerHTML = "Tu es" + "<br>" + insulte1 [Math.floor(Math.random()*insulte1.length)]+ " " + insulte2 [Math.floor(Math.random()*insulte2.length)] + "<br>" + "!!! " + "<br>" + "&nbsp;"
 }
